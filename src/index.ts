302df4a294d8cd8cc0ec64c86771e2cba2b88316
Snider <snider@lt.hn>
2022-03-26 13:40:01 +0000

    adds zoneid and zone key
     purge if the id for a zone is set

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import uploader from './uploader';
+import purge from './purge';
 import { getInput, setFailed, info } from '@actions/core';
 import { join } from 'path';
 import { Utils } from '@technote-space/github-action-helper';
@@ -8,8 +9,13 @@ async function run() {
     const source = join(Utils.getWorkspace(), getInput('source'));
     const storageZoneName = getInput('storageZoneName');
     const accessKey = getInput('accessKey');
+    const zoneId = getInput('zoneID');
+    const zoneKey = getInput('zoneKey');
     info(`Deploying ${source}`);
     await uploader(source, storageZoneName, accessKey);
+    if(zoneID){
+      await purge(zoneID, zoneKey);
+    }
   } catch (error) {
     setFailed(error);
   }
