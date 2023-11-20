09d7f53ebd0bbd5833cdbaf92d9dff8d7bdcb4bb
Kyle Reeves <kyreeves@icloud.com>
2023-11-20 00:45:00 -0600

    Update

--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,27 @@ import { getInput, setFailed, info } from '@actions/core';
 import { join } from 'path';
 import { Utils } from '@technote-space/github-action-helper';
 
-
 async function run() {
   try {
     const source = join(Utils.getWorkspace(), getInput('source'));
     const storageZoneName = getInput('storageZoneName');
     const accessKey = getInput('accessKey');
     const zoneId = getInput('zoneId');
-    const zoneKey = getInput('zoneKey');
     const url = getInput('url');
-
 
     if(storageZoneName && accessKey) {
       info(`Deploying ${source}`);
       await uploader(source, storageZoneName, accessKey);
     }
 
-    if(zoneId && zoneKey) {
-      if (url) {
-        info(`Purging ${url}`);
-        await purgeUrl(zoneKey, url);
-      } else {
-        info(`Purging ${source}`);
-        await purge(zoneId, zoneKey);
-      }
+    if(accessKey && zoneId) {
+      info(`Purging ${source}`);
+      await purge(zoneId, accessKey);
+    }
+
+    if(accessKey && url) {
+      info(`Purging ${url}`);
+      await purgeUrl(accessKey, url);
     }
   } catch (error) {
     setFailed(error);
